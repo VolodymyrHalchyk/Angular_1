ba93535fb9e440f03ca44408b0a515ca83d7c7ca
VolodymyrHalchyk <ckatbiha1991@gmail.com>
2021-01-25 22:48:48 +0200

    Lesson_1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { CarComponent } from './components/cars/car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    CarsComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule
